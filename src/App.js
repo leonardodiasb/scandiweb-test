8a84ee716c5b0b463c2c6d9356c29ed61c849be5
leonardo <leonardodias@ucl.br>
2022-04-26 14:56:37 -0300

    Create navbar component, Add basic style to navbar. Create ApolloClient. Query navbar categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
 import { Provider } from 'react-redux';
+import { withApollo } from '@apollo/client/react/hoc';
 import store from './redux/configureStore';
-import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+const NavbarWithClient = withApollo(Navbar);
 
 function App() {
   return (
     <>
-      <Provider store={store} />
+      <Provider store={store}>
+        <NavbarWithClient />
+      </Provider>
     </>
   );
 }
