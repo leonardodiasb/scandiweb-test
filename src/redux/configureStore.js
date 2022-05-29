1b6dcfcbee1ad0f6350046b1806fda6a6e0917dd
leonardo <leonardodias@ucl.br>
2022-05-29 16:00:38 -0300

    Add cart reducer to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import currencyReducer from './reducers/currencyReducer';
 import categoriesReducer from './reducers/categoriesReducer';
+import cartReducer from './reducers/cartReducer';
 
 export default configureStore({
   reducer: {
     currency: currencyReducer,
     categories: categoriesReducer,
+    cart: cartReducer,
   },
 });
