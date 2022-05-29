96024679c2e3c995c403d74b212b283c4c642a90
leonardo <leonardodias@ucl.br>
2022-05-29 16:00:56 -0300

    Create cart constants

---
+++ src/redux/constants/cart.constants.js
@@ -0,0 +1,9 @@
+const cartConstants = {
+  ADD_TO_CART: 'cart/addToCart',
+  REMOVE_FROM_CART: 'cart/removeFromCart',
+  INCREMENT_AMOUNT: 'cart/incrementAmount',
+  DECREMENT_AMOUNT: 'cart/decrementAmount',
+  CHANGE_ATTRIBUTE: 'cart/changeAttribute',
+};
+
+export default cartConstants;
