9074fb985fd190839b8d01744cf8ffe9dccab391
leonardo <leonardodias@ucl.br>
2022-05-30 14:15:23 -0300

    Create checkDuplicateProduct function

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -4,13 +4,31 @@ const initialState = {
   cart: [],
 };
 
+function checkDuplicateProduct(state, action) {
+  const duplicateFilter = state.cart.filter((product) => (
+    product.id === action.payload.id
+  ));
+  const cartWithoutDuplicate = state.cart.filter((product) => (
+    product.id !== action.payload.id
+  ));
+  if (duplicateFilter.length) {
+    const newAmount = { amount: duplicateFilter[0].amount + 1 };
+    const newProduct = { ...duplicateFilter[0], ...newAmount };
+    return {
+      ...state,
+      cart: [...cartWithoutDuplicate, newProduct],
+    };
+  }
+  return {
+    ...state,
+    cart: [...state.cart, action.payload],
+  };
+}
+
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case cartConstants.ADD_TO_CART:
-      return {
-        ...state,
-        cart: [...state.cart, ...action.cart],
-      };
+      return checkDuplicateProduct(state, action);
     default:
       return state;
   }
