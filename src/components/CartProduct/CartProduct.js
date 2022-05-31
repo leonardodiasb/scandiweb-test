c985e5a729b1a97d543f4e840b068cf65c017c7f
leonardo <leonardodias@ucl.br>
2022-05-31 20:43:01 -0300

    Add cart menu classes to CartProduct component

--- src/components/CartProduct/CartProduct.js
+++ src/components/CartProduct/CartProduct.js
@@ -15,10 +15,6 @@ class CartProduct extends Component {
     };
   }
 
-  componentDidMount() {
-    console.log('MOUNT');
-  }
-
   changeAttribute(attr, item) {
     const { product } = this.props;
     const payload = {
@@ -47,41 +43,47 @@ class CartProduct extends Component {
   }
 
   render() {
-    const { product, products, currency } = this.props;
+    const {
+      product, products, currency, cartMenu,
+    } = this.props;
     const productInfo = products.filter((p) => p.id === product.id);
     return (
-      <div className="product-cart-row">
-        <div className="product-info-container">
-          <div className="name-container">
-            <div className="product-brand">{productInfo[0].brand}</div>
-            <div className="product-name">{productInfo[0].name}</div>
+      <div className={cartMenu ? 'menu-cart-row' : 'product-cart-row'}>
+        <div className={cartMenu ? 'menu-info-container' : 'product-info-container'}>
+          <div className={cartMenu ? 'menu-name-container' : 'name-container'}>
+            <div className={cartMenu ? 'menu-product-brand' : 'product-brand'}>{productInfo[0].brand}</div>
+            <div className={cartMenu ? 'menu-product-name' : 'product-name'}>{productInfo[0].name}</div>
           </div>
-          <div className="price-amount">
+          <div className={cartMenu ? 'menu-price-amount' : 'price-amount'}>
             {currency}
             {productInfo[0].prices.filter((price) => price.currency.symbol === currency)[0].amount}
           </div>
-          <div className="attributes-container">
+          <div className={cartMenu ? 'menu-attributes-container' : 'attributes-container'}>
             {product.attributes.length ? (
               <div>
                 {productInfo[0].attributes.map((attribute) => (
-                  <div className="attribute-box-cart" key={attribute.id}>
-                    <div className="attribute-name">
+                  <div className={cartMenu ? 'menu-attribute-box' : 'attribute-box-cart'} key={attribute.id}>
+                    <div className={cartMenu ? 'menu-attribute-name' : 'attribute-name'}>
                       {attribute.name}
                       :
                     </div>
                     {(attribute.type === 'swatch') ? (
-                      <ul className="attribute-list">
+                      <ul className={cartMenu ? 'menu-attribute-list' : 'attribute-list'}>
                         {attribute.items.map((item) => (
                           <li key={item.id} className={product.attributes.filter((att) => att.id === attribute.id)[0].item.id === item.id ? 'color-box-wrapper-active' : 'color-box-wrapper'}>
-                            <div className="color-box" style={{ backgroundColor: item.value }} onClick={() => { this.changeAttribute(attribute.id, item.id); }} aria-hidden="true" />
+                            <div className={cartMenu ? 'menu-color-box' : 'color-box'} style={{ backgroundColor: item.value }} onClick={() => { this.changeAttribute(attribute.id, item.id); }} aria-hidden="true" />
                           </li>
                         ))}
                       </ul>
                     ) : (
-                      <ul className="attribute-list">
+                      <ul className={cartMenu ? 'menu-attribute-list' : 'attribute-list'}>
                         {attribute.items.map((item) => (
                           <li key={item.id}>
-                            <button className={product.attributes.filter((att) => att.id === attribute.id)[0].item.id === item.id ? 'item-box-active' : 'item-box'} type="button" onClick={() => { this.changeAttribute(attribute.id, item.id); }}>{item.value}</button>
+                            {cartMenu ? (
+                              <button className={product.attributes.filter((att) => att.id === attribute.id)[0].item.id === item.id ? 'menu-item-box-active' : 'menu-item-box'} type="button" onClick={() => { this.changeAttribute(attribute.id, item.id); }}>{item.value}</button>
+                            ) : (
+                              <button className={product.attributes.filter((att) => att.id === attribute.id)[0].item.id === item.id ? 'item-box-active' : 'item-box'} type="button" onClick={() => { this.changeAttribute(attribute.id, item.id); }}>{item.value}</button>
+                            )}
                           </li>
                         ))}
                       </ul>
@@ -93,20 +95,20 @@ class CartProduct extends Component {
               <div />
             )}
           </div>
-          <button type="button" className="exclude-button" onClick={() => { this.remove(product); }}>Exclude</button>
+          <button type="button" className={cartMenu ? 'menu-exclude-button' : 'exclude-button'} onClick={() => { this.remove(product); }}>Exclude</button>
         </div>
-        <div className="amount-image-container">
-          <div className="amount-box">
-            <button type="button" className="increment-button" onClick={() => { this.increment(product); }}>
-              <span className="vertical-vector" />
-              <span className="horizontal-vector" />
+        <div className={cartMenu ? 'menu-amount-image-container' : 'amount-image-container'}>
+          <div className={cartMenu ? 'menu-amount-box' : 'amount-box'}>
+            <button type="button" className={cartMenu ? 'menu-increment-button' : 'increment-button'} onClick={() => { this.increment(product); }}>
+              <span className={cartMenu ? 'menu-vertical-vector' : 'vertical-vector'} />
+              <span className={cartMenu ? 'menu-horizontal-vector' : 'horizontal-vector'} />
             </button>
-            <div className="product-amount">{product.amount}</div>
-            <button type="button" className="decrement-button" onClick={() => { this.decrement(product); }} disabled={product.amount === 0}>
-              <span className="horizontal-vector" />
+            <div className={cartMenu ? 'menu-product-amount' : 'product-amount'}>{product.amount}</div>
+            <button type="button" className={cartMenu ? 'menu-decrement-button' : 'decrement-button'} onClick={() => { this.decrement(product); }} disabled={product.amount === 0}>
+              <span className={cartMenu ? 'menu-horizontal-vector' : 'horizontal-vector'} />
             </button>
           </div>
-          <div className="image-box">
+          <div className={cartMenu ? 'menu-image-box' : 'image-box'}>
             <img src={productInfo[0].gallery[0]} alt={productInfo[0].name} />
           </div>
         </div>
@@ -121,6 +123,11 @@ CartProduct.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   products: PropTypes.arrayOf(PropTypes.any).isRequired,
   currency: PropTypes.string.isRequired,
+  cartMenu: PropTypes.bool,
+};
+
+CartProduct.defaultProps = {
+  cartMenu: false,
 };
 
 const mapStateToProps = (state) => ({
