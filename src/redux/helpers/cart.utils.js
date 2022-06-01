9965282cea3d17133442b94db301a593c01c8b8a
leonardo <leonardodias@ucl.br>
2022-06-01 19:32:31 -0300

    Move helper functions from cartreducer

---
+++ src/redux/helpers/cart.utils.js
@@ -0,0 +1,91 @@
+const updateObject = (oldObject, newValues) => ({ ...oldObject, ...newValues });
+
+const updateItemInArray = (array, itemId, updateItemCallback) => {
+  const updatedItems = array.map((item) => {
+    if (item.id !== itemId) {
+      return item;
+    }
+
+    const updatedItem = updateItemCallback(item);
+    return updatedItem;
+  });
+
+  return updatedItems;
+};
+
+const filterObject = (arr, id) => arr.filter((product) => (
+  product.id === id
+))[0];
+
+export const increment = (state, action) => {
+  const filtered = filterObject(state.cart, action.payload.id);
+  const newAmount = { amount: filtered.amount + 1 };
+  const newCartState = updateItemInArray(state.cart, action.payload.id, (i) => (
+    updateObject(i, newAmount)
+  ));
+  return {
+    ...state,
+    cart: [...newCartState],
+  };
+};
+
+export const decrement = (state, action) => {
+  const filtered = filterObject(state.cart, action.payload.id);
+  const newAmount = { amount: filtered.amount - 1 };
+  const newCartState = updateItemInArray(state.cart, action.payload.id, (i) => (
+    updateObject(i, newAmount)
+  ));
+  return {
+    ...state,
+    cart: [...newCartState],
+  };
+};
+
+export const editAttribute = (state, action) => {
+  updateItemInArray(
+    action.payload.productAttributes, action.payload.newAttribute.id, (i) => (
+      updateObject(i, action.payload.newAttribute)),
+  );
+  const newAttributes = {
+    attributes: updateItemInArray(
+      action.payload.productAttributes, action.payload.newAttribute.id, (i) => (
+        updateObject(i, action.payload.newAttribute)),
+    ),
+  };
+  const newCartState = updateItemInArray(state.cart, action.payload.productId, (i) => (
+    updateObject(i, newAttributes)
+  ));
+
+  return {
+    ...state,
+    cart: [...newCartState],
+  };
+};
+
+export const addProduct = (state, action) => {
+  const duplicateProduct = filterObject(state.cart, action.payload.id);
+  if (duplicateProduct) {
+    const newAmount = { amount: duplicateProduct.amount + 1 };
+    const newProduct = updateObject(duplicateProduct, newAmount);
+
+    const newCartState = updateItemInArray(state.cart, action.payload.id, (i) => (
+      updateObject(i, newProduct)
+    ));
+    return {
+      ...state,
+      cart: [...newCartState],
+    };
+  }
+  return {
+    ...state,
+    cart: [...state.cart, action.payload],
+  };
+};
+
+export const removeProduct = (state, action) => {
+  const newCartState = state.cart.filter((product) => product.id !== action.payload.id);
+  return {
+    ...state,
+    cart: [...newCartState],
+  };
+};
--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,104 +1,11 @@
 import cartConstants from '../constants/cart.constants';
+import {
+  addProduct, decrement, editAttribute, increment, removeProduct,
+} from '../helpers/cart.utils';
 
 const initialState = {
   cart: [],
 };
-
-function updateObject(oldObject, newValues) {
-  return { ...oldObject, ...newValues };
-}
-
-function updateItemInArray(array, itemId, updateItemCallback) {
-  const updatedItems = array.map((item) => {
-    if (item.id !== itemId) {
-      return item;
-    }
-
-    const updatedItem = updateItemCallback(item);
-    return updatedItem;
-  });
-
-  return updatedItems;
-}
-
-function filterObject(arr, id) {
-  return arr.filter((product) => (
-    product.id === id
-  ))[0];
-}
-
-function increment(state, action) {
-  const filtered = filterObject(state.cart, action.payload.id);
-  const newAmount = { amount: filtered.amount + 1 };
-  const newCartState = updateItemInArray(state.cart, action.payload.id, (i) => (
-    updateObject(i, newAmount)
-  ));
-  return {
-    ...state,
-    cart: [...newCartState],
-  };
-}
-
-function decrement(state, action) {
-  const filtered = filterObject(state.cart, action.payload.id);
-  const newAmount = { amount: filtered.amount - 1 };
-  const newCartState = updateItemInArray(state.cart, action.payload.id, (i) => (
-    updateObject(i, newAmount)
-  ));
-  return {
-    ...state,
-    cart: [...newCartState],
-  };
-}
-
-function editAttribute(state, action) {
-  updateItemInArray(
-    action.payload.productAttributes, action.payload.newAttribute.id, (i) => (
-      updateObject(i, action.payload.newAttribute)),
-  );
-  const newAttributes = {
-    attributes: updateItemInArray(
-      action.payload.productAttributes, action.payload.newAttribute.id, (i) => (
-        updateObject(i, action.payload.newAttribute)),
-    ),
-  };
-  const newCartState = updateItemInArray(state.cart, action.payload.productId, (i) => (
-    updateObject(i, newAttributes)
-  ));
-
-  return {
-    ...state,
-    cart: [...newCartState],
-  };
-}
-
-function addProduct(state, action) {
-  const duplicateProduct = filterObject(state.cart, action.payload.id);
-  if (duplicateProduct) {
-    const newAmount = { amount: duplicateProduct.amount + 1 };
-    const newProduct = updateObject(duplicateProduct, newAmount);
-
-    const newCartState = updateItemInArray(state.cart, action.payload.id, (i) => (
-      updateObject(i, newProduct)
-    ));
-    return {
-      ...state,
-      cart: [...newCartState],
-    };
-  }
-  return {
-    ...state,
-    cart: [...state.cart, action.payload],
-  };
-}
-
-function removeProduct(state, action) {
-  const newCartState = state.cart.filter((product) => product.id !== action.payload.id);
-  return {
-    ...state,
-    cart: [...newCartState],
-  };
-}
 
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
